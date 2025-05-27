3b88759de08f63af79ff8b85238cf5b06f889ed0
Максим Фокеев <maksolyuks@gmail.com>
2025-05-27 07:59:10 +0400

    Небольшие правки.

--- Danplom/DataBaseConnector/DataBase.cs
+++ Danplom/DataBaseConnector/DataBase.cs
@@ -22,120 +22,7 @@ namespace Danplom.DataBaseConnector
             return _instance ??= new DataBase();
         }
 
-        // TODO Убрать лишний код
-        #region
-        //public IEnumerable<ExecutorViewModel>? GetExecutorCollection()
-        //{
-        //    using (MySqlConnection connection = new MySqlConnection(_connectionString))
-        //    {
-        //        try
-        //        {
-        //            var queryString =
-        //            """
-        //            select id as Id,
-        //            executor_name as Name
-        //            from executors
-        //            """;
 
-        //            return connection.Query<ExecutorViewModel>(queryString);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            MessageBox.Show("Не удалось загрузить данные.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-        //            return null;
-        //        }
-        //    }
-        //}
-        //public IEnumerable<MaterialViewModel>? GetMaterialCollection()
-        //{
-        //    using (MySqlConnection connection = new MySqlConnection(_connectionString))
-        //    {
-        //        try
-        //        {
-        //            var queryString =
-        //            """
-        //            select id as Id,
-        //            material_name as Name,
-        //            stock_quantity as StockQuantity,
-        //            measurement_unit as MeasurementUnit
-        //            from materials
-        //            """;
-
-        //            return connection.Query<MaterialViewModel>(queryString);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            MessageBox.Show("Не удалось загрузить данные.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-        //            return null;
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Изменяет в базе данных исполнителя и статус заданной заявки.
-        ///// В случае ошибки при попытке изменения данных появится сообщение об ошибке.
-        ///// </summary>
-        ///// <param name="updatedRequest">Обновленная заявка.</param>
-        ///// <returns>Возвращает true, если изменение данных прошло успешно, иначе возвращает false.</returns>
-        //public bool ChangeRequest(RequestViewModel updatedRequest)
-        //{
-        //    using (MySqlConnection connection = new MySqlConnection(_connectionString))
-        //    {
-        //        try
-        //        {
-        //            var queryString =
-        //            $"""
-        //            update requests
-        //            set requests.executor_id = '{updatedRequest?.Executor.Id}',
-        //            requests.request_status = '{(int)updatedRequest?.Status}'
-        //            where requests.id = '{updatedRequest?.Id}'
-        //            """;
-
-        //            connection.Execute(queryString);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            MessageBox.Show("Не удалось обновить заявку.", "Ошибка сохранения!", MessageBoxButton.OK, MessageBoxImage.Error);
-        //            return false;
-        //        }
-
-        //        return true;
-        //    }
-        //}
-
-        ///// <summary>
-        ///// Изменяет в базе данных количество запасов заданного материала.
-        ///// В случае ошибки при попытке изменения данных появится сообщение об ошибке.
-        ///// </summary>
-        ///// <param name="changedMaterial">Изменяемый материал.</param>
-        ///// <param name="changingQuantity">Число, меняющее количество StockQuantity у материала.</param>
-        ///// <returns>Возвращает true, если изменение данных прошло успешно, иначе возвращает false.</returns>
-        //public bool ChangeMaterialStockQuantity(MaterialViewModel changedMaterial, int changingQuantity)
-        //{
-        //    using (MySqlConnection connection = new MySqlConnection(_connectionString))
-        //    {
-        //        try
-        //        {
-        //            if (changedMaterial.StockQuantity + changingQuantity < 0) return false;
-
-        //            var queryString =
-        //            $"""
-        //            update materials
-        //            set materials.stock_quantity = '{changedMaterial.StockQuantity + changingQuantity}'
-        //            where materials.id = '{changedMaterial.Id}'
-        //            """;
-
-        //            connection.Execute(queryString);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            MessageBox.Show("Не удалось обновить данные материала.", "Ошибка сохранения!", MessageBoxButton.OK, MessageBoxImage.Error);
-        //            return false;
-        //        }
-
-        //        return true;
-        //    }
-        //}
-        #endregion
         /// <summary>
         /// Проверяет есть ли пользователь в базе данных.
         /// </summary>
@@ -174,41 +61,6 @@ namespace Danplom.DataBaseConnector
             {
                 MessageBox.Show("Такого пользователя не существует.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
                 return null;
-            }
-        }
-        public IEnumerable<RequestDto>? GetRequestCollection()
-        {
-            using (MySqlConnection connection = new MySqlConnection(_connectionString))
-            {
-                try
-                {
-                    var queryString =
-                    """
-                    select requests.id as Id,
-                    details.id as detailId,
-                    details.detail_name as detailName,
-                    requests.required_quantity as RequiredQuantity,
-                    requests.time_to_complete as TimeToComplete,
-                    users.id as userId,
-                    users.user_name as userName,
-                    users.user_role as role,
-                    users.login as login,
-                    users.password as password,
-                    requests.request_status as status
-                    from requests
-                    left join details
-                    ON requests.detail_id = details.id
-                    left join users
-                    ON requests.user_id = users.id
-                    """;
-
-                    return connection.Query<RequestDto>(queryString);
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Не удалось загрузить данные.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return null;
-                }
             }
         }
         /// <summary>
@@ -270,7 +122,72 @@ namespace Danplom.DataBaseConnector
                 return true;
             }
         }
+        /// <summary>
+        /// Изменяет в базе данных статус заданной заявки.
+        /// В случае ошибки при попытке изменения данных появится сообщение об ошибке.
+        /// </summary>
+        /// <param name="updatedRequest">Обновленная заявка.</param>
+        /// <returns>Возвращает true, если изменение данных прошло успешно, иначе возвращает false.</returns>
+        public bool ChangeRequest(RequestDto updatedRequest)
+        {
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                try
+                {
+                    var queryString =
+                    $"""
+                    update requests
+                    set requests.request_status = '{(int)updatedRequest?.Status}'
+                    where requests.id = '{updatedRequest?.Id}'
+                    """;
 
+                    connection.Execute(queryString);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Не удалось обновить заявку.", "Ошибка сохранения!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        public IEnumerable<RequestDto>? GetRequestCollection()
+        {
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                try
+                {
+                    var queryString =
+                    """
+                    select requests.id as Id,
+                    details.id as detailId,
+                    details.detail_name as detailName,
+                    requests.required_quantity as RequiredQuantity,
+                    requests.time_to_complete as TimeToComplete,
+                    users.id as userId,
+                    users.user_name as userName,
+                    users.user_role as role,
+                    users.login as login,
+                    users.password as password,
+                    requests.request_status as status
+                    from requests
+                    left join details
+                    ON requests.detail_id = details.id
+                    left join users
+                    ON requests.user_id = users.id
+                    """;
+
+                    return connection.Query<RequestDto>(queryString);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Не удалось загрузить данные.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return null;
+                }
+            }
+        }
         public IEnumerable<DetailDto>? GetDetailCollection()
         {
             using (MySqlConnection connection = new MySqlConnection(_connectionString))
@@ -320,7 +237,6 @@ namespace Danplom.DataBaseConnector
                 }
             }
         }
-
         public IEnumerable<RequestDto>? GetCurrentExecutorRequestCollection(UserDto currentUser)
         {
             using (MySqlConnection connection = new MySqlConnection(_connectionString))
@@ -355,31 +271,6 @@ namespace Danplom.DataBaseConnector
                     MessageBox.Show("Не удалось загрузить данные.", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
                     return null;
                 }
-            }
-        }
-
-        public bool ChangeRequest(RequestDto updatedRequest)
-        {
-            using (MySqlConnection connection = new MySqlConnection(_connectionString))
-            {
-                try
-                {
-                    var queryString =
-                    $"""
-                    update requests
-                    set requests.request_status = '{(int)updatedRequest?.Status}'
-                    where requests.id = '{updatedRequest?.Id}'
-                    """;
-
-                    connection.Execute(queryString);
-                }
-                catch (Exception)
-                {
-                    MessageBox.Show("Не удалось обновить заявку.", "Ошибка сохранения!", MessageBoxButton.OK, MessageBoxImage.Error);
-                    return false;
-                }
-
-                return true;
             }
         }
     }
